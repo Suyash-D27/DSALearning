40cc5fdd9441fb4a9bccb94b54de33e73d99f8f9
Suyash-D27 <yashdaharwal27@gmail.com>
2025-06-17 22:55:03 +0530

    updated

--- src/arrays/searchingAlgorithm/linearSearch.java
+++ src/arrays/searchingAlgorithm/linearSearch.java
@@ -1,9 +1,11 @@
 package arrays.searchingAlgorithm;
 
+import java.util.Arrays;
+
 public class linearSearch {
     public static void main(String[] args) {
-        int[]array={1,2,5,9,76,98,65,89,67};
-            int traget = 98;
+//        int[]array={1,2,5,9,76,98,65,89,67};
+//            int traget = 98;
 //            int ans=searchAlgo(array,traget);
 //        System.out.println(ans);
 //        String name="suyash";
@@ -12,9 +14,26 @@ public class linearSearch {
 //        int ans2 = searchInRange(array,traget,3,7);
 //        System.out.println(ans2);
 
-        int [] formin ={23,23,456,67,76,34,56,78};
-        int ans3 = min(formin);
-        System.out.println(ans3);
+//        int [] formin ={23,23,456,67,76,34,56,78};
+//        int ans3 = min(formin);
+//        System.out.println(ans3);
+
+
+//        int [][] arr2 = {
+//                {2,3,45,5},
+//                {1,5,6,1,4},
+//                {4,8}
+//        };
+//
+//        int traget3= 5;
+//        int []ans4=searchIn2DArraya(arr2,traget3);
+//        System.out.println(Arrays.toString(ans4));
+//
+//        System.out.println(MaxIn2DArray(arr2));
+
+        int []array5={1,2,4,5,6,7,3,5,8,10,34,54,87};
+        int[]ans5=EvenNumbersOnly(array5);
+        System.out.println(Arrays.toString(ans5));
     }
 
 
@@ -67,5 +86,52 @@ public class linearSearch {
         }
         return ans;
     }
+
+    static int[] searchIn2DArraya(int [][] arr,int traget){
+
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                if (arr[row][col]==traget) return new int[] {row,col};
+            }
+        }
+        return new int[]{-1,-1};
+
+    }
+
+    static int MaxIn2DArray(int [][] arr){
+            int MAX=Integer.MIN_VALUE;
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                if (arr[row][col]>MAX) {
+                    MAX = arr[row][col];
+                }
+            }
+        }
+        return MAX;
+
+    }
+
+    static int[] EvenNumbersOnly (int[] arr){
+
+        int [] temp =new int[0];
+
+        for (int i = 0; i <arr.length ; i++) {
+            if(arr[i]%2==0) {
+                int[] temp2 = new int[temp.length + 1];
+
+                for (int j = 0; j < temp.length; j++) {
+                    temp2[j] =temp[j];
+                }
+                temp2[temp.length]=arr[i];
+
+                temp=temp2;
+            }
+        }
+
+
+        return temp;
+    }
+
+
 
 }
